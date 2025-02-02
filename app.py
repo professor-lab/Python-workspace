000b1c45f27843d93afea7f952cef8fe1f8c4f33
professor-lab <arjansambad@gmail.com>
2025-02-02 11:01:06 +0530

    import date time

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///8anna.db"
+db=SQLAlchemy(app)
 
-
+class Todo(db.model):
+    sno=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),primary_key=False)
+    date_created=db.Column(db.DateTime,defaut=datetime.utcnow)
 
 @app.route("/")
 def hello_world():
