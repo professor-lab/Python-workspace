78c06d9827bb36603cd4fd8b0b1f1b10a01e13de
professor-lab <arjansambad@gmail.com>
2025-02-02 11:07:58 +0530

    finaly app.py update

--- app.py
+++ app.py
@@ -7,11 +7,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///8anna.db"
 db=SQLAlchemy(app)
 
-class Todo(db.model):
+class Todo(db.Model):
     sno=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(200),nullable=False)
-    desc=db.Column(db.String(500),primary_key=False)
-    date_created=db.Column(db.DateTime,defaut=datetime.utcnow)
+    desc=db.Column(db.String(500),nullable=False)
+    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self)->str:
+        return f"{self.sno} - {self.title}"
 
 @app.route("/")
 def hello_world():
