1e1ec30d67a1854b66332b44e2d1b89ca97add46
professor-lab <arjansambad@gmail.com>
2025-02-08 16:26:47 +0530

    app update

--- Auth/app.py
+++ Auth/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
 
 @app.route('/')
 def index():
