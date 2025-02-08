84dc9870a0e34c949f155cd2cf5e9f0c6a8c979c
professor-lab <arjansambad@gmail.com>
2025-02-08 16:08:44 +0530

    app py

--- Auth/app.py
+++ Auth/app.py
@@ -12,6 +12,11 @@ def register():
         #reuest handal
         pass   
     return render_template("register.html")
+@app.route('/login',method=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    return render_template('login.html')
     
 
 
