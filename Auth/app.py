38b1b025890b9e96617082f884e5aed2b9db8eea
professor-lab <arjansambad@gmail.com>
2025-02-10 15:23:19 +0530

    Integer and String are Capital

--- Auth/app.py
+++ Auth/app.py
@@ -11,7 +11,7 @@ class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(100), nullable=False)
     email=db.Column(db.String(100),unique=True)
-    password=db.Column(db.string(100))
+    password=db.Column(db.String(100))
 
     def __init__(self,name,email,password):
         self.name=name
