ff1dc7931f201c79448800314e8085037c660a00
professor-lab <arjansambad@gmail.com>
2025-02-10 15:22:14 +0530

    app update

--- Auth/app.py
+++ Auth/app.py
@@ -9,7 +9,7 @@ db=SQLAlchemy(app)
 
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(100),nullable=False)
+    name=db.Column(db.String(100), nullable=False)
     email=db.Column(db.String(100),unique=True)
     password=db.Column(db.string(100))
 
