dc93c54919ad4466f1614f2f49e7b0f1e784bc30
professor-lab <arjansambad@gmail.com>
2025-03-06 16:35:49 +0530

    student console project

--- student-management/student.py
+++ student-management/student.py
@@ -25,7 +25,37 @@ class StudentManager:
 
 	def remove_student(self,student_id):
 		if student_id in self.students:
-			
+			del self.students[student_id]
+			print("Student Remove")
+		else:
+			print("Student Not Found")
+
+def student_main():
+	manager=StudentManager()
+	while True:
+		print("\n1. Add Student \n2. Remove Student \n3. enroll in courses \n4. Exit")
+		choice=input("Enter Choice")
+		if choice=='4':
+			break
+		elif choice=='1':
+			student_id=input("Student ID: ")
+			name=input("name: ")
+			manager.add_student(student_id,name)
+		elif choice=='2':
+			student_id=input("Student ID remove:")
+			manager.remove_student(student_id)
+		elif choice=='3':
+			student_id=input("Student ID:")
+			course=input("Course:")
+			if student_id in manager.students:
+				manager.students[student_id].enroll(course)
+			else:
+				print("Student Not Found")
+		else:
+			print("invalid choice")
+
+student_main()
+
 
 			
 		
