c8fd1e0dc8bb8dfe87378aea6eb717893cd93d5c
professor-lab <arjansambad@gmail.com>
2025-03-09 13:24:41 +0530

    file write

---
+++ file-operation/file_line_read.py
@@ -0,0 +1,4 @@
+filename='pi-digit.txt'
+with open(filename) as file_object:
+	for line in file_object:
+		print(line.rstrip())
---
+++ file-operation/file_list_line.py
@@ -0,0 +1,6 @@
+filename='pi-digit.txt'
+with open(filename) as file_object:
+	lines=file_object.readlines()
+
+for line in lines:
+	print(line.rstrip())
