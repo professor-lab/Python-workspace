c755658372a966d3cb3405f35807f9a4e0a2c5c4
professor-lab <arjansambad@gmail.com>
2025-03-12 17:43:44 +0530

    foods

--- Py/02-list/foods.py
+++ Py/02-list/foods.py
@@ -1,1 +1,8 @@
-my_foods=['pizza','falafel']
+my_foods=['pizza','falafel','carrot cake']
+friend_foods=my_foods[:]
+
+print("my my_foods :")
+print(my_foods)
+
+print("friend food:")
+print(friend_foods)
